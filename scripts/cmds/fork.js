2428cc0b9a14b99443834a35e52ab8321ec32b6c
Mohammad Maruf <yourmarufb@gmail.com>
2026-09-11 23:00:01 +0600

    Update scripts/cmds/fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -3,29 +3,25 @@ const remoteCommand = require("../../remoteCommand");
 const API_URL = "https://mohammad-maruf.vercel.app";
 
 module.exports = {
-  config: {
-    name: "fork",
-    version: "1.0.0",
-    author: "𝐌𝐚𝐑𝐮𝐅",
-    countDown: 0,
-    role: 0,
+ config: {
+ name: "fork",
+ aliases: ["repository"],
+ version: "1.0.0",
+ author: "𝐌𝐚𝐑𝐮𝐅",
+ countDown: 0,
+ role: 0,
 
-    shortDescription: "Fork Link",
-    longDescription: "Responds when fork or repository is mentioned.",
-    category: "config",
+ shortDescription: "Fork Link",
+ longDescription: "Show fork information.",
+ category: "config",
 
-    guide: {
-      en: "Type 'fork' or 'repository'"
-    }
-  },
+ guide: {
+ en: "{pn}"
+ }
+ },
 
-  onStart: remoteCommand(
-    API_URL,
-    "fork"
-  ).onStart,
-
-  onChat: remoteCommand(
-    API_URL,
-    "fork"
-  ).onChat
+ onStart: remoteCommand(
+ API_URL,
+ "fork"
+ ).onStart
 };
