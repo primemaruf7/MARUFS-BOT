3d933156bb3cc2130d12d8bcd4778d8f8605e7cb
Mohammad Maruf <yourmarufb@gmail.com>
2026-09-11 09:14:00 +0600

    Update scripts/cmds/kiss2.js

--- scripts/cmds/kiss2.js
+++ scripts/cmds/kiss2.js
@@ -21,7 +21,7 @@ module.exports = {
     version: "1.7",
     author: "MahMUD",
     role: 0,
-    category: "fun",
+    category: "love",
     cooldown: 8,
     guide: "kiss2 [mention/reply/UID]",
   },
